f8e7dad52b49b8d36f476b22de7fae7b65b7f2ec
Maxus94 <kmaxusk@gmail.com>
2024-01-22 15:09:43 +0200

    ready

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const apiKey = '40858721-2ab2962236a746e97c71283b6';
 const form = document.querySelector('.search-form');
 const listOfPictures = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
+const loader = document.querySelector('.loader-block');
 
 const gallerySimpleLightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
@@ -26,26 +26,25 @@ async function submitHandler(event) {
   page = 1;
   textToSearch = form.elements.searchText.value.trim();
   iziToast.destroy();
-  if (textToSearch === '') {
-    alert(
-      "Search form can't be empty, enter there some text for images search."
-    );
-    return;
+  if (textToSearch === '') {        
+    alert("Search form can't be empty, enter there some text for images search.");
+    return
   } else {
     event.preventDefault();
     listOfPictures.innerHTML = '';
+    loader.classList.remove('hidden');
     loadMoreButton.classList.add('hidden');
     try {
       const data = await servicePictures();
       if (Array.from(data.data.hits).length === 0) {
-        iziToast.show({
-          title: 'Error',
+        iziToast.show({          
           message: `Sorry, there are no images matching your search query. Please try again.`,
           close: false,
           backgroundColor: 'red',
           messageColor: 'white',
           messageSize: 20,
-          timeout: 2000,
+          timeout: 5000,
+          maxWidth: '400px',
           position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
         });
       } else if (Array.from(data.data.hits).length < 40) {
@@ -53,35 +52,17 @@ async function submitHandler(event) {
           'beforeend',
           createMarkup(data.data.hits)
         );
-        gallerySimpleLightbox.refresh();
-        iziToast.show({
-          //title: 'Error',
-          message: `Hooray! We found ${data.data.totalHits} images.`,
-          close: false,
-          backgroundColor: 'green',
-          messageColor: 'white',
-          messageSize: 20,
-          timeout: 2000,
-          position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
-        });
+        gallerySimpleLightbox.refresh();        
+        form.reset();
       } else {
         loadMoreButton.classList.remove('hidden');
         listOfPictures.insertAdjacentHTML('beforeend', createMarkup(data.data.hits));
         gallerySimpleLightbox.refresh();
-        iziToast.show({
-          //title: 'Error',
-          message: `Hooray! We found ${data.data.totalHits} images.`,
-          close: false,
-          backgroundColor: 'green',
-          messageColor: 'white',
-          messageSize: 20,
-          timeout: 2000,
-          position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
-        });
+        form.reset();
       }
     } catch {
       iziToast.show({
-        //title: 'Error',
+        title: 'Error',
         message: `Something goes wrong, please try reload page.`,
         close: false,
         backgroundColor: 'red',
@@ -91,18 +72,19 @@ async function submitHandler(event) {
         position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
       });
     }
+    loader.classList.add('hidden');
   }
 }
 
 async function loadMoreHandler() {
     page += 1;
     iziToast.destroy();
+    loader.classList.remove('hidden');
     try {
       const data = await servicePictures(page);
       if ((page + 1) * 40 >= data.data.totalHits) {
         loadMoreButton.classList.add('hidden');
         iziToast.show({
-          //title: 'Error',
           message: `End of the collection of "${textToSearch}" exceeded.`,
           close: false,
           backgroundColor: 'red',
@@ -115,10 +97,7 @@ async function loadMoreHandler() {
       listOfPictures.insertAdjacentHTML('beforeend', createMarkup(data.data.hits));
       gallerySimpleLightbox.refresh();
       const galleryCard = document.querySelector('.photo-card');
-      console.log(galleryCard);
-      const rect = galleryCard.getBoundingClientRect();
-      console.log(rect);
-      //window.scrollBy(0, (rect.height * 3) + 48);
+      const rect = galleryCard.getBoundingClientRect();      
       window.scrollBy({
         top: rect.height * 2 + 24,
         behavior: "smooth",
@@ -135,6 +114,7 @@ async function loadMoreHandler() {
         position: 'topRight', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
       });
     }
+    loader.classList.add('hidden');
   }
 
 async function servicePictures(page = 1) {
